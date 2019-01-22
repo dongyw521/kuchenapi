f8c0fb0d78f8f2506334287e1aefd3839b2ed706
dongyanwei <dongyanwei@autohome.com.cn>
2019-01-22 09:36:04 +0800

    1

--- src/kuchen.EntityFrameworkCore/Migrations/20190116094956_Init.Designer.cs
+++ src/kuchen.EntityFrameworkCore/Migrations/20190116094956_Init.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using kuchen.EntityFrameworkCore;
 
 namespace kuchen.Migrations
 {
     [DbContext(typeof(kuchenDbContext))]
-    partial class kuchenDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190116094956_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/kuchen.EntityFrameworkCore/Migrations/20190116112017_Init3.Designer.cs
+++ src/kuchen.EntityFrameworkCore/Migrations/20190116112017_Init3.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using kuchen.EntityFrameworkCore;
 
 namespace kuchen.Migrations
 {
     [DbContext(typeof(kuchenDbContext))]
-    partial class kuchenDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190116112017_Init3")]
+    partial class Init3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
